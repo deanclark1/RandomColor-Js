b93b33ce6ee2b903a9d8e54247e0cee52ae1a653
deanclark1 <ntumydoreen@gmail.com>
2022-02-28 13:08:05 +0000

    added if color is dark, change text to white

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const button = document.querySelector('button');
 const h1 = document.querySelector('h1');
 button.addEventListener('click', function() {
     const newColor = makeRandomColor();
-    
+
     document.body.style.backgroundColor = newColor;
     h1.innerText = newColor;
 
@@ -12,5 +12,10 @@ const makeRandomColor = () =>{
     const r = Math.floor(Math.random() * 255);
     const g = Math.floor(Math.random() * 255);
     const b = Math.floor(Math.random() * 255);
+
+    if( (r + g + b) <= 250 ) {
+        h1.style.color = 'white';
+    }
     return ` rgb(${r}, ${g}, ${b})`;
 }
+
